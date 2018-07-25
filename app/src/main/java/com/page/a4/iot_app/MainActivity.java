1a2340338372fba88525f0351abdb5dd35504d1e
wony <zxwvk8@naver.com>
2018-07-25 17:06:14 +0900

    프로젝트 수정11

--- app/src/main/java/com/page/a4/iot_app/MainActivity.java
+++ app/src/main/java/com/page/a4/iot_app/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        //작성 1
+        //수정1
     }
 
 }
