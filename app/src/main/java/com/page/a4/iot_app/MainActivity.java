010273750d6b14884804309a207f46480a943699
wony <zxwvk8@naver.com>
2018-07-27 16:00:44 +0900

    통신 기능 테스트 1

--- app/src/main/java/com/page/a4/iot_app/MainActivity.java
+++ app/src/main/java/com/page/a4/iot_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.page.a4.iot_app;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -16,12 +19,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     Button button_send;
     EditText editText_send;
+    EditText editText_ip;
     TextView textView_list;
+
+    //서버주서
+    public static final String sIP = "192.168.1.131";
+    //사용할 통신 포트
+    public static final int sPORT = 9000;
+    //데이터 보낼 클랙스
+    public SendData mSendData = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,20 +71,71 @@ public class MainActivity extends AppCompatActivity
 
         button_send = (Button)findViewById(R.id.button_Send);
         editText_send = (EditText)findViewById(R.id.editText);
+        editText_ip = (EditText)findViewById(R.id.editText_ip);
         textView_list = (TextView)findViewById(R.id.textView_List);
+
+
+
+        // StrictMode는 개발자가 실수하는 것을 감지하고 해결할 수 있도록 돕는 일종의 개발 툴
+        // - 메인 스레드에서 디스크 접근, 네트워크 접근 등 비효율적 작업을 하려는 것을 감지하여
+        //   프로그램이 부드럽게 작동하도록 돕고 빠른 응답을 갖도록 함, 즉  Android Not Responding 방지에 도움
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+
+
 
         button_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String str = editText_send.getText().toString();
-                String str_old = textView_list.getText().toString();
-                textView_list.setText(str_old + "\n" + str);
+                //SendData 클래스 생성
+                mSendData = new SendData();
+                //보내기 시작
+                mSendData.start();
             }
         });
 
 
 
     }
+
+    //데이터 보내는 쓰레드 클래스
+    class SendData extends Thread{
+        public void run(){
+            try{
+                //UDP 통신용 소켓 생성
+                DatagramSocket socket = new DatagramSocket();
+                //서버 주소 변수
+                InetAddress serverAddr = InetAddress.getByName(editText_ip.getText().toString());
+
+                //보낼 데이터 생성
+                //byte[] buf = ("Hello World").getBytes();
+                byte[] buf = editText_send.getText().toString().getBytes();
+
+
+                //패킷으로 변경
+                DatagramPacket packet = new DatagramPacket(buf, buf.length, serverAddr, sPORT);
+
+                //패킷 전송!
+                socket.send(packet);
+
+                //데이터 수신 대기
+                socket.receive(packet);
+                //데이터 수신되었다면 문자열로 변환
+                String msg = new String(packet.getData());
+
+                //txtView에 표시
+                textView_list.append(msg);
+            }catch (Exception e){
+
+            }
+        }
+    }
+
+
+
+
 
     @Override
     public void onBackPressed() {
